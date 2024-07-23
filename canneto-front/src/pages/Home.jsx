047025a88f1c50d01634d9aa1db321cd4f40a572
Cianuro-83 <buzzeosimone@yahoo.com>
2024-07-23 21:24:33 +0200

    feat: BOTTONI HOME PAGE

--- canneto-front/src/pages/Home.jsx
+++ canneto-front/src/pages/Home.jsx
@@ -2,15 +2,28 @@ const Home = () => {
 	return (
 		<div className="rfc-Home">
 			<div className="bande">
-				<h2 className="text-uppercase text-center">
+				<h2 className="text-uppercase text-center mt-5 fw-bolder">
 					basilica - santuario regionale
 				</h2>
 			</div>
-			<div className="img-container">
-				<img src="/img/MADONNA.webp" alt="Madonna" className="sfondo" />
+			<div className="centrale">
+				<button
+					type="button"
+					className="btn btn-success btn-lg orari text-uppercase fw-bolder">
+					orari
+				</button>
+
+				<div className="img-container">
+					<img src="/img/MADONNA.webp" alt="Madonna" className="sfondo" />
+				</div>
+				<button
+					type="button"
+					className="btn btn-success btn-lg entra text-uppercase fw-bolder">
+					entra
+				</button>
 			</div>
 			<div className="bande py-3">
-				<h2 className="text-uppercase text-center">
+				<h2 className="text-uppercase text-center mb-5 fw-bolder">
 					Santa maria di canneto
 				</h2>
 			</div>
