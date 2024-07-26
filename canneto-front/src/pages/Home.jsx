fdd9f3cc106b05ac04fbd7186c7c2f35ce91f353
Cianuro-83 <buzzeosimone@yahoo.com>
2024-07-26 16:11:57 +0200

    fix: :fire: home refactor

--- canneto-front/src/pages/Home.jsx
+++ canneto-front/src/pages/Home.jsx
@@ -1,13 +1,13 @@
+import { Link } from 'react-router-dom';
+
 export default function Home() {
 	return (
 		<>
 			<div className="text-uppercase text-center fw-bolder titolo">
-				<img src="/img/OMBRELLO.svg" alt="" />
 				<h1>
 					basilica - santuario regionale <br />
 					santa maria di canneto
 				</h1>
-				<img src="/img/OMBRELLO.svg" alt="" />
 			</div>
 			<div className="holy">
 				<img src="/img/MADONNA.webp" alt="" className="madonna" />
@@ -19,11 +19,13 @@ export default function Home() {
 					orari
 				</button>
 
-				<button
-					type="button"
-					className="btn btn-primary text-uppercase fw-bolder btn-lg">
-					entra nel sito
-				</button>
+				<Link to="/app/realhome">
+					<button
+						type="button"
+						className="btn btn-primary text-uppercase fw-bolder btn-lg">
+						entra nel sito
+					</button>
+				</Link>
 			</div>
 		</>
 	);
