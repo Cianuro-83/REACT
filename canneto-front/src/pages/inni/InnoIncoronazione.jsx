519866ce46da3306341935a424cd0921f8c1bb21
Cianuro-83 <buzzeosimone@yahoo.com>
2024-07-29 10:03:22 +0200

    feat: inno incoronazione

--- canneto-front/src/pages/inni/InnoIncoronazione.jsx
+++ canneto-front/src/pages/inni/InnoIncoronazione.jsx
@@ -4,6 +4,80 @@ const InnoIncoronazione = () => {
 			<h3 className="text-center text-uppercase mt-3 fw-bolder">
 				inno dell'incoronazione
 			</h3>
+			<p>
+				L'8 dicembre del 1953, S. Ecc.za mons. Biagio Musto, vescovo di
+				Aquino, Sora e Pontecorvo, con una "Notificazione" al clero e ai
+				fedeli delle tre diocesi, dava il lieto annunzio del 1° Congresso
+				Mariano Interdiocesano (12-19 settembre 1954) e dell'incoronazione
+				della Madonna di Canneto da parte del Capitolo Vaticano nella città
+				Sora. Al Maestro Padre Rodrigo Di Rocco fu dato mandato di comporre
+				l'Inno ufficiale per l'incoronazione su versi elaborati da mons.
+				Gaetano Squilla
+			</p>
+			<div className="container">
+				<div className="testo">
+					<p className="strofa">
+						Lassù, solinga nella tua dimora, <br />
+						Tu per mille anni udisti il nostro pianto, <br />
+						dolce ascoltasti della fede il canto <br />
+						che a Te saliva di perenne amor. <br />
+					</p>
+					<p className="ritornello fw-bolder">
+						Rit. O di Canneto Vergine Signora, <br />
+						che fosti e sei la nostra Madre buona, <br />
+						scendi dai monti e cingi la corona <br />
+						che ti daremo sfavillante d’or. <br />
+					</p>
+					<p className="strofa">
+						Venimmo a schiere al trono benedetto, <br />
+						a Te speranza della nostra vita, <br />
+						fosti e sei sempre il faro che ci addita <br />
+						la meta eterna d’olezzanti fior. <br />
+						<p className="ritornello">
+							<br /> Rit.
+						</p>
+						Di vetta in vetta disse le tue lodi <br />
+						l’alto, rupestre, candido Appennino, <br />
+						all’ubertosa Valle di Comino <br />
+						splendente rise l’Astro tuo del ciel. <br />
+						<p className="ritornello">
+							<br /> Rit.
+						</p>
+						Dal picciol Melfa, che Ti sgorga accanto, <br />
+						dalle città che bagna il verde Liri, <br />
+						dall’uno all’altro mar dolci sospiri <br />
+						vennero e canti d’alme a Te fedel. <br />
+						<p className="ritornello">
+							<br /> Rit.
+						</p>
+						Or vieni Tu dal monte nella Valle <br />
+						Nell’anno alle tue lodi consacrato, <br />
+						vieni a mostrare il viso tuo beato, <br />
+						e gli occhi belli amati da Gesù. <br />
+						<p className="ritornello">
+							<br /> Rit.
+						</p>
+						Se venne l’aspra lotta della vita, <br />
+						nel cuore tuo che sente ogni dolore, <br />
+						trovammo risi di materno amore, <br />
+						invito al bene e sprone alla virtù. <br />
+						<p className="ritornello">
+							<br /> Rit.
+						</p>
+						Celeste fiore, giglio immacolato, <br />
+						vieni a parlare della tua purezza, <br />
+						vieni a ridire, o eterna giovinezza, <br />
+						le grazie che a Te diede il Creator. <br />
+						<p className="ritornello">
+							<br /> Rit.
+						</p>
+						Al mondo tristo, che tormenta il male, <br />
+						la pace dona, luce di speranza, <br />
+						fa che sentiamo tutti la fragranza <br />
+						sublime e dolce del Divino Amor. <br />
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 };
